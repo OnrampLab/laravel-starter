451f5d168ac872e7e91fe90ba520034c72913c9c
Kos <koshuang@gmail.com>
2019-03-21 17:15:59 +0800

    chore(package): [hello] added as a example package

---
+++ packages/hello/src/HelloServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types = 1);
+namespace Hello;
+use Illuminate\Support\ServiceProvider;
+class HelloServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerCommands();
+    }
+    // --------------------------------------------------------------------------------
+    //  private
+    // --------------------------------------------------------------------------------
+    /**
+     * Register the Artisan commands.
+     *
+     * @return void
+     */
+    protected function registerCommands()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Console\Commands\SayHiCommand::class,
+            ]);
+        }
+    }
+}
