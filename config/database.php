be8eccf436fe3ca4b82e66242e166bec0df98b18
Kos <koshuang@gmail.com>
2020-02-11 15:08:47 +0800

    chore(config): [circleci] use mysql for ci testing

--- config/database.php
+++ config/database.php
@@ -92,6 +92,22 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', false),
         ],
 
+        'testing_mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_TEST_DATABASE', 'testing_db'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'sticky' => true,
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
+
     ],
 
     /*
