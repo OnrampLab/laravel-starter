8481d9aad70f75760722c7b6b24f144c1ccd5015
Kos <koshuang@gmail.com>
2020-06-05 12:03:00 +0800

    chore(config): [auth] use jwt as api auth

--- config/auth.php
+++ config/auth.php
@@ -37,12 +37,12 @@ return [
 
     'guards' => [
         'web' => [
-            'driver' => 'session',
+            'driver' => 'jwt',
             'provider' => 'users',
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'jwt',
             'provider' => 'users',
             'hash' => false,
         ],
