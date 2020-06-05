e6cc4da1bd3bb1f60cce75dcccdf0548b88977c8
Kos <koshuang@gmail.com>
2020-06-05 11:16:15 +0800

    chore(config): set up laravel-auditing and add custom user resolver

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        OwenIt\Auditing\AuditingServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
