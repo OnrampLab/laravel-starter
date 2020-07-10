a2946b89b94cf332a4d55ceae9e84b4ff9399020
Kos <koshuang@gmail.com>
2020-07-10 18:00:40 +0800

    db(auth/seed): [RoleSeederTableSeeder] added

--- Modules/Auth/Database/Seeders/AuthDatabaseSeeder.php
+++ Modules/Auth/Database/Seeders/AuthDatabaseSeeder.php
@@ -16,6 +16,6 @@ class AuthDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $this->call(RoleSeederTableSeeder::class);
     }
 }
