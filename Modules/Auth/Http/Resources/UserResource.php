6f4bc0416e6947185633243cebd0626fcfa23290
Eric Chang <ericsonc510@gmail.com>
2020-07-22 23:32:35 +0800

    feat(auth/resource): [UserResource] added

---
+++ Modules/Auth/Http/Resources/UserResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\Auth\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+use Modules\Auth\Http\Resources\RoleResource;
+use Modules\Account\Http\Resources\AccountResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'roles' => RoleResource::collection($this->roles),
+            'accounts' => AccountResource::collection($this->accounts()->orderBy('id', 'ASC')->get()),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
