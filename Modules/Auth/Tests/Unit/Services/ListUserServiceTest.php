69827192bf07657a726fb33ffb47cb4bb60773ff
Eric Chang <ericsonc510@gmail.com>
2020-07-23 23:15:37 +0800

    feat(auth/service): [ListUserService] added

---
+++ Modules/Auth/Tests/Unit/Services/ListUserServiceTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Modules\Auth\Tests;
+
+use Mockery;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Modules\Auth\Entities\User;
+use Modules\Auth\Repositories\UserRepository;
+use Modules\Auth\Services\ListUserService;
+
+class ListUserServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        $this->repositoryMock = Mockery::mock(UserRepository::class);
+        $this->app->instance(UserRepository::class, $this->repositoryMock);
+        $this->service = app(ListUserService::class);
+    }
+
+    /**
+     * @test
+     *
+     */
+    public function perform_should_work()
+    {
+        $this->repositoryMock
+            ->shouldReceive('all')
+            ->once()
+            ->andReturn(collect([$this->user]));
+
+        $this->service->perform();
+    }
+}
