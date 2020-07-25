ac7c04a566582c5c16ff3665dd9f5a48ee900a23
Eric Chang <ericsonc510@gmail.com>
2020-07-25 13:55:40 +0800

    feat(auth/service): [DeleteUserService] added

---
+++ Modules/Auth/Tests/Unit/Services/DeleteUserServiceTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Modules\Auth\Tests;
+
+use Mockery;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Modules\Auth\Entities\User;
+use Modules\Auth\Repositories\UserRepository;
+use Modules\Auth\Services\DeleteUserService;
+
+class DeleteUserServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+        $this->repositoryMock = Mockery::mock(UserRepository::class);
+        $this->app->instance(UserRepository::class, $this->repositoryMock);
+        $this->service = app(DeleteUserService::class);
+    }
+
+    /**
+     * @test
+     *
+     */
+    public function perform_should_work()
+    {
+        $this->repositoryMock
+            ->shouldReceive('delete')
+            ->once()
+            ->withArgs(function ($userId) {
+                return $userId === $this->user->id;
+            })
+            ->andReturn(true);
+
+        $this->service->perform($this->user->id);
+    }
+}
