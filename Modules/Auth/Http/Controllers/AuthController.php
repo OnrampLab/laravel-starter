ecd2093fb732f27c0f830bf50dce0fc8d7f10da7
Kos <koshuang@gmail.com>
2020-12-21 16:49:00 +0800

    fix(auth/api): [/api/auth/me] did not wrap with data

--- Modules/Auth/Http/Controllers/AuthController.php
+++ Modules/Auth/Http/Controllers/AuthController.php
@@ -65,7 +65,7 @@ class AuthController extends Controller
      */
     public function me()
     {
-        return response()->json(new UserResource(auth()->user()));
+        return new UserResource(auth()->user());
     }
 
     /**
