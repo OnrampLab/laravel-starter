30bbab8c7946f6cf854505ac04df4f6a875c8823
Christopher Albert <christopherA08@hotmail.com>
2021-10-08 15:19:30 +0800

    fix(test): [ExampleTest] disable mix

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        $this->withoutmix();
         $response = $this->get('/');
 
         $response->assertStatus(200);
